3ec55083c63578b072b15e3eb08d37f2c48d41f5
Aigerim Dzhanyshpekova <107724350+AikaJan@users.noreply.github.com>
2024-10-02 14:48:47 -0700

    Dynamic port path

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const { router } = require("./routes");
 
 const app = express();
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 // Настройка CORS с несколькими разрешенными источниками
 const allowedOrigins = [
