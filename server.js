aa6d4e7acbe5680daa65c4a5325e7705d050df8f
Aigerim Dzhanyshpekova <107724350+AikaJan@users.noreply.github.com>
2024-10-01 21:48:12 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,38 +1,3 @@
-// require("dotenv").config();
-
-// const express = require("express");
-// const cors = require("cors");
-// const mongoose = require("mongoose");
-// const { router } = require("./routes");
-
-// const app = express();
-// const PORT = 3001;
-
-// mongoose.connect(
-//   process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/mealplans_db",
-//   {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   }
-// );
-
-// const db = mongoose.connection;
-// db.on("error", console.error.bind(console, "MongoDB connection error:"));
-// db.once("open", () => {
-//   console.log("Connected to MongoDB");
-// });
-
-// app.use(cors());
-// app.use(express.json());
-
-// app.use("/", router);
-
-// const server = app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
-
-// module.exports = app;
-
 require("dotenv").config();
 
 const express = require("express");
@@ -41,9 +6,28 @@ const mongoose = require("mongoose");
 const { router } = require("./routes");
 
 const app = express();
-const PORT = process.env.PORT || 3001;
 
-// Подключение к MongoDB
+const PORT = 3001;
+
+// Настройка CORS с несколькими разрешенными источниками
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://your-production-domain.com",
+];
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: "GET,POST", // Разрешенные HTTP методы
+    allowedHeaders: "Content-Type,Authorization", // Разрешенные заголовки
+  })
+);
+
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/mealplans_db",
   {
@@ -58,27 +42,13 @@ db.once("open", () => {
   console.log("Connected to MongoDB");
 });
 
-// Настройка CORS для разрешения запросов с клиента на Vercel
-app.use(
-  cors({
-    origin: "https://ai-generator-mealplan-client.vercel.app", // Разрешаем запросы с Vercel-клиента
-    methods: "GET,POST", // Разрешаем только методы GET и POST
-    credentials: true,  // Если необходимо работать с авторизацией или куки
-  })
-);
-
-app.use(express.json()); // Для обработки JSON в запросах
-
-// Подключение роутов
+app.use(express.json());
 app.use("/", router);
 
-// Запуск сервера
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
 module.exports = app;
 
-
-module.exports = app;
 
